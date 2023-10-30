1f34943b3e4a5d78647cd41678cf6946406d7418
Marissa Vargas <marissa.lorena@hotmail.com>
2023-10-29 21:45:38 -0600

    chore: added schema validation with Joi for env vars

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,17 +1,17 @@
 // Import core libraries
-import { Module } from '@nestjs/common';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { Module } from '@nestjs/common'
+import { ThrottlerModule } from '@nestjs/throttler'
+import { MongooseModule } from '@nestjs/mongoose'
+import * as Joi from 'joi'
 
 // Import config files
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { throttlerConfig } from '@config/index';
-import { mongoConfig } from '@config/mongo.config';
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { throttlerConfig } from '@config/index'
+import { mongoConfig } from '@config/mongo.config'
 
 // Import own app files
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
 
 @Module({
   imports: [
@@ -19,6 +19,12 @@ import { MongooseModule } from '@nestjs/mongoose';
       isGlobal: true,
       cache: true,
       load: [throttlerConfig, mongoConfig],
+      validationSchema: Joi.object({
+        LISTENING_PORT: Joi.number().valid(4000).required(),
+        THROTTLE_TTL: Joi.number().required(),
+        THROTTLE_LIMIT: Joi.number().less(15).required(),
+        MONGODB_URI: Joi.string().required()
+      })
     }),
     // Rate limit protection
     ThrottlerModule.forRootAsync({
